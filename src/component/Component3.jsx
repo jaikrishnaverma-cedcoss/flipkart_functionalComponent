9c70c621f009e11e41b00c0fc7e47415c035dcd0
jaikrishnaverma <jaikrishnaverma>
2022-08-24 17:30:25 +0530

    flipkart

--- src/component/Component3.jsx
+++ src/component/Component3.jsx
@@ -4,6 +4,7 @@ import { CarouselData } from "./CarouselData";
       
      const Component3=()=> { 
       const [marke,setmarke] = useState(0)
+      const [val,setVal]=useState(0)
       const nextSlide = () => {
       let newSlide =
         marke === CarouselData.length - 1
@@ -29,8 +30,15 @@ import { CarouselData } from "./CarouselData";
         <div className="rowbox crousal">
             <div className='absolute'><button onClick={prevSlide}><i className="fa fa-angle-left" aria-hidden="true"></i></button><button onClick={nextSlide}><i className="fa fa-angle-right" aria-hidden="true"></i></button></div>
       {/* <Slider index={marke} /> */}
-      <div className='crbox'>
-       <img src={`banner${marke}.webp`} style={{width:"98%"}}  alt="This is a carousel slide" />
+      <div className={`crbox boxActive${marke}`} style={{display:"flex",width:"300%"}}>
+
+      {
+        CarouselData.map((slide,index)=>{
+          return <img className="image" src={`banner${index}.webp`} style={{width:"100%"}}  alt="This is a carousel slide" />;
+        })
+      }
+     
+       
        </div>
         </div>
     </div> );
